76d73aefef3f9b6a08701982a9da8f874203d45c
Oscar Heimbrecht <heimbrecht@campus.tu-berlin.de>
2022-11-15 18:43:05 +0100

    Code cleanup

--- website/index.php
+++ website/index.php
@@ -14,58 +14,44 @@
 </head>
 
 <body>
-<header>
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-white">
-    <div class="container-fluid">
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#navbarExample01"
-        aria-controls="navbarExample01"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarExample01">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item active">
-            <a class="nav-link" aria-current="page" href="#">Home/Search</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Users</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Courses</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Competencies</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!-- Navbar -->
-
-  <!-- Header image -->
-  
-  <!-- <div
-    class="p-5 bg-image"
-    style="
-      background-image: url('../resources/header.png');
-	  width: 80%;
-    "> -->
-    
-  <!-- Header image -->
-</header>
+	<header>
+		<!-- Navbar -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-white">
+			<div class="container-fluid">
+				<button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01" aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
+					<i class="fas fa-bars"></i>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarExample01">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item active">
+							<a class="nav-link" aria-current="page" href="#">Home/Search</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Users</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Courses</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Competencies</a>
+						</li>
+					</ul>
+					<ul class="navbar-nav ms-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="">User ABC</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<!-- Navbar -->
+	</header>
 	<div class="container">
-	<img src="../resources/header.png" alt="HeaderImage" class="center" style="width:100%;">
+		<img src="../resources/header.png" alt="HeaderImage" class="center" style="width:100%;">
 	</div>
 	<hr>
 	<div class="container">
-		<!-- input tag -->
+
 
 
 
@@ -80,46 +66,43 @@
 			</form>
 		</div>
 
-		<!-- <div class="row" id="products">
-			<div class="row" id="filter"> -->
-				<form>
-					<div class="form-group row">
-						<div class="form-group col-md-3">
-							<select data-filter="provider" class="filter-provider filter form-control">
-								<option value="">Select Provider</option>
-								<option value="">Show All</option>
-							</select>
-						</div>
-						<div class="form-group col-md-2">
-							<select data-filter="price" class="filter-price filter form-control">
-								<option value="">Select Price Range</option>
-								<option value="">Show All</option>
-							</select>
-						</div>
-						<div class="form-group col-md-3">
-							<select data-filter="location" class="filter-location filter form-control">
-								<option value="">Select Location</option>
-								<option value="">Show All</option>
-							</select>
-						</div>
-						<div class="form-group col-md-2">
-							<select data-filter="start-date" class="filter-start-date filter form-control">
-								<option value="">Select Start Date</option>
-								<option value="">Show All</option>
-							</select>
-						</div>
-						<div class="form-group col-md-2">
-							<select data-filter="duration" class="filter-duration filter form-control">
-								<option value="">Select Duration</option>
-								<option value="">Show All</option>
-							</select>
-						</div>
-						
-					</div>
-				</form>
-			<!-- </div>
-		</div> -->
-		<!-- ordered list -->
+
+		<form>
+			<div class="form-group row">
+				<div class="form-group col-md-3">
+					<select data-filter="provider" class="filter-provider filter form-control">
+						<option value="">Select Provider</option>
+						<option value="">Show All</option>
+					</select>
+				</div>
+				<div class="form-group col-md-2">
+					<select data-filter="price" class="filter-price filter form-control">
+						<option value="">Select Price Range</option>
+						<option value="">Show All</option>
+					</select>
+				</div>
+				<div class="form-group col-md-3">
+					<select data-filter="location" class="filter-location filter form-control">
+						<option value="">Select Location</option>
+						<option value="">Show All</option>
+					</select>
+				</div>
+				<div class="form-group col-md-2">
+					<select data-filter="start-date" class="filter-start-date filter form-control">
+						<option value="">Select Start Date</option>
+						<option value="">Show All</option>
+					</select>
+				</div>
+				<div class="form-group col-md-2">
+					<select data-filter="duration" class="filter-duration filter form-control">
+						<option value="">Select Duration</option>
+						<option value="">Show All</option>
+					</select>
+				</div>
+
+			</div>
+		</form>
+
 		<ol id='list'>
 			<?php
 			$json = file_get_contents('http://course-api-service/');
@@ -129,19 +112,7 @@
 				echo "<li class='course'>$course</li>";
 			}
 			?>
-			<!-- <li class="courses">Cat</li>
-		<li class="courses">Dog</li>
-		<li class="courses">Elephant</li>
-		<li class="courses">Fish</li>
-		<li class="courses">Gorilla</li>
-		<li class="courses">Monkey</li>
-		<li class="courses">Turtle</li>
-		<li class="courses">Whale</li>
-		<li class="courses">Aligator</li>
-		<li class="courses">Donkey</li>
-		<li class="courses">Horse</li> -->
 		</ol>
-
 
 </body>
 
