612c38fbdfd73b91ddd8789e2604d856f3087333
Oscar Heimbrecht <heimbrecht@campus.tu-berlin.de>
2022-12-04 17:47:44 +0100

    add selections

--- website/index.php
+++ website/index.php
@@ -10,7 +10,10 @@
 	<link rel="stylesheet" type="text/css" href="./css/style.css">
 	<script src="./js/script.js"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/css/bootstrap-multiselect.css" />
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/js/bootstrap-multiselect.min.js"></script>
 </head>
 
 <body>
